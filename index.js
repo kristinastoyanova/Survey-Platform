9257577d1549501552a539304a49f13bac682d78
kristina_ss <kristina@gmail.com>
2017-11-15 22:37:11 +0200

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send({hi: 'there'});
+    res.send({bye: 'buddy'});
 });
 
 //use PORT in prod environment when the app is deployed in
